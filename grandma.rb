f8bbab2fa307209245d4c5c1cf565f0e58920e6e
Jessica Gladstone <jcg321@comcast.net>
2018-09-24 18:49:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,9 +12,9 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(arg)
-  if arg = "I LOVE YOU GRANDMA!"
+  if arg == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO, PUMPKIN!"
-  elseif arg = arg.upcase
+  elseif arg ==arg.upcase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
